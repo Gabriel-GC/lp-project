82159f67c1d8809d995b5104a2588f9e10fec950
GabrielGC <goncalves.cunhaa@gmail.com>
2024-08-24 18:54:49 -0300

    render-theme add

--- src/templates/Home/App.test.jsx
+++ src/templates/Home/App.test.jsx
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import { renderTheme } from '../../styles/render-theme';
 import Home from '.';
 
+
 test('renders learn react link', () => {
-  render(<Home />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  renderTheme(<Home />);
 });
