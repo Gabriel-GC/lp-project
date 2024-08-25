dd2a978ce35dce40553d25ee52327d61943fd535
GabrielGC <goncalves.cunhaa@gmail.com>
2024-08-25 14:28:36 -0300

    added fonts

--- src/styles/global-syles.js
+++ src/styles/global-syles.js
@@ -1,14 +1,21 @@
-import { createGlobalStyle, css } from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
+
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap');
 * {
 margin: 0;
 padding: 0;
 }
 
-body {
-  ${({ theme }) => css`
-  /* background: ${theme.colors.secondaryColor}; */
-  `}
+html {
+  font-size:62.5%;
+}
+body{
+  font-size: 1.6rem;
+  font-family: 'Open Sans', sans-serif;
+}
+h1 {
+  font-family: 'Montserrat', sans-serif;
 }
 `;
